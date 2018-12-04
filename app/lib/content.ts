a4d738ae4d96aeb0cf278175a7fc617dfe24c293
Christian Dupuis <cd@atomist.com>
2018-12-04 16:03:33 +0100

    Move legal files over to app
    [changelog:added]

--- app/lib/content.ts
+++ app/lib/content.ts
@@ -1,3 +1,5 @@
+import * as fs from "fs-extra";
+import * as path from "path";
 import * as showdown from "showdown";
 import { downloadFile } from "./git";
 
@@ -14,4 +16,17 @@ export async function getFile(name: string, process: (content: string) => Promis
     }
 }
 
+<<<<<<< HEAD
+=======
+export async function getLocalFile(name: string, process: (content: string) => Promise<string> = MarkdownConverter): Promise<string> {
+    if (files.has(name)) {
+        return files.get(name);
+    } else {
+        const content = await process((await fs.readFile(path.join(".", name))).toString());
+        files.set(name, content);
+        return content;
+    }
+}
+
+>>>>>>> Move legal files over to app
 export const MarkdownConverter = async (content: string) => converter.makeHtml(content);
--- app/lib/policy.ts
+++ app/lib/policy.ts
@@ -1,14 +1,14 @@
 import * as express from "express";
-import { getFile } from "./content";
+import { getLocalFile } from "./content";
 
 export const policyRouter = express.Router();
 
 policyRouter.get("/cookie-policy.html", async (req: express.Request, res: express.Response) => {
-    const policy = await getFile("legal/COOKIE_POLICY.md");
+    const policy = await getLocalFile("legal/COOKIE_POLICY.md");
     res.render("policy", { policy });
 });
 
 policyRouter.get("/privacy-policy.html", async (req: express.Request, res: express.Response) => {
-    const policy = await getFile("legal/PRIVACY_POLICY.md");
+    const policy = await getLocalFile("legal/PRIVACY_POLICY.md");
     res.render("policy", { policy });
 });
